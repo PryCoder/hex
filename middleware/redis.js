0515c9f147f022acf18ef3a53595dca5188cbfdb
Priyanshu Gupta <priyanshugupta007007@gmail.com>
2025-12-24 10:16:26 +0530

    first response

---
+++ middleware/redis.js
@@ -0,0 +1,19 @@
+import { redis } from "../index.js";
+
+export const getCachedData = (key) => async (req, res, next) => {
+  try {
+    const cached = await redis.get(key);
+
+    if (cached) {
+      console.log("cached");
+      // STOP execution if cached
+      return res.json({ products: JSON.parse(cached) });
+    }
+
+    // No cache, continue to route handler
+    next();
+  } catch (err) {
+    console.error(err);
+    next(); // still continue if Redis fails
+  }
+};
