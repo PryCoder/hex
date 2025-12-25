aed79668cd3f3aa534be56025e00cb468d377889
Priyanshu Gupta <priyanshugupta007007@gmail.com>
2025-12-25 18:43:37 +0530

    added vite imports

--- clie/src/App.jsx
+++ clie/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   useEffect(() => {
     let isMounted = true;
 
-    fetch(`${process.env.VITE_API_URL}/products`)
+    fetch(`${import.meta.env.VITE_API_URL}/products`)
       .then((res) => res.json())
       .then((data) => {
         if (isMounted) {
@@ -35,7 +35,7 @@ const App = () => {
   const fetchProductDetail = (id) => {
     setDetailLoading(true);
 
-    fetch(`${process.env.VITE_API_URL}/product/${id}`)
+    fetch(`${import.meta.env.VITE_API_URL}/product/${id}`)
       .then((res) => res.json())
       .then((data) => {
         setSelectedProduct(data.productDetail[0]);
